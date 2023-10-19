59d3110cba1b650e31b9fcb2bc4915f7c4118e57
Micah Ives <micbenives@gmail.com>
2023-10-18 20:22:45 -0500

    Add div ID counter, local storage functionality for game cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ function fetchGames(query) {
       })
       .then(function (data) {
         renderGames(data);
+        console.log(data);
       });
   }
 
@@ -23,20 +24,43 @@ function fetchGames(query) {
 function renderGames(data) {
     const gameContainer = $('#game-container');
 
+    // counter variable for cards
+    let cardCount = 1;
+
     // count the cards generated, add unique ID, count++
     // for loop for generating cards-- unique IDs to local storage to wishlist
 
     data.results.forEach(function(game) {
         const gameDiv = $('<div></div>');
         gameDiv.addClass('game-card');
+
+        // Generates unique id for game div using counter variable
+        const uniqueId = `card-${cardCount}`;
+        gameDiv.attr('id', uniqueId);
+
+        // Array to hold game genres--this needs some work
+        const gameGenres = [];
+        gameGenres.push(game.genres);
+        console.log(gameGenres);
+
         gameDiv.append(
             $('<h2></h2>').text(game.name),
             $('<img>').attr('src', game.background_image)
         );
+
+        // When you click, send to local storage
+        gameDiv.on('click', function() {
+          localStorage.setItem(`favoritedGame_${game.name}`, uniqueId);
+        })
+
+        cardCount++;
+
         gameContainer.append(gameDiv);
     });
 }
 
+
 $(document).ready(function() {
     fetchGames();
 });
+
