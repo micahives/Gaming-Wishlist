dd69491ef3be4efb9bdb4481be9bdd03fb0f3606
Micah Ives <micbenives@gmail.com>
2023-10-19 20:37:21 -0500

    Add wishlistButton, testlocalstorage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,25 +32,29 @@ function renderGames(data) {
 
     data.results.forEach(function(game) {
         const gameDiv = $('<div></div>');
-        gameDiv.addClass('game-card');
+        gameDiv.addClass('card');
 
         // Generates unique id for game div using counter variable
         const uniqueId = `card-${cardCount}`;
         gameDiv.attr('id', uniqueId);
 
         // Array to hold game genres--this needs some work
-        const gameGenres = [];
-        gameGenres.push(game.genres);
-        console.log(gameGenres);
+        // const gameGenres = [];
+        // gameGenres.push(game.genres);
+        // console.log(gameGenres);
+
+        // Add wishlist button
+        const wishlistButton = $('<button></button>').text('Add to Wishlist');
 
         gameDiv.append(
             $('<h2></h2>').text(game.name),
-            $('<img>').attr('src', game.background_image)
+            $('<img>').attr('src', game.background_image),
+            wishlistButton
         );
 
-        // When you click, send to local storage
-        gameDiv.on('click', function() {
-          localStorage.setItem(`favoritedGame_${game.name}`, uniqueId);
+        // When you click, send to local storage, need as a data object as mitchell suggested so we can render that game in the wishlist
+        wishlistButton.on('click', function() {
+          localStorage.setItem(`favoritedGame_${game.name}`, JSON.stringify(game));
         })
 
         cardCount++;
@@ -63,7 +67,31 @@ function renderGames(data) {
 $(document).ready(function() {
     fetchGames();
 });
- 
 
 
-window.addEventListener("load", setup)
+// Separate function to style the cards, will update with Bulma classes, run this function..
+// when gameDivs are dynamically generated
+function styleCards() {
+  var gameCard = $('<div></div>');
+  var gameImage = $('<img>');
+  var gameTitle = $('<h3></h3>');
+
+  gameCard.addClass('card');
+  gameCard.append(
+    gameTitle.text(game.name),
+    gameImage.attr('src', game.background_image)
+  );
+}
+
+// See if we can put a simple card in local storage and render it in the wishlist
+function localStorageTest() {
+  var testObject = {
+    name: 'John Doe',
+    age: 30
+  };
+  var stringTestObj = JSON.stringify(testObject);
+  localStorage.setItem('TestObject', stringTestObj);
+}
+
+localStorageTest();
+// window.addEventListener("load", setup) 
